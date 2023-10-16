a04d264e721d3543c3a6e0f01a8544a64d73fd88
Harijohnson <harijn72@gmail.com>
2023-10-16 11:33:34 +0530

    cart and checkout functinalityes are done and total cart values finding functions are done

--- store/models.py
+++ store/models.py
@@ -40,17 +40,35 @@ class Order(models.Model):
 
      def __str__(self):
         return str(self.id)
+     
+     @property  # total amt of order total amt in top of cart page
+     def get_cart_total(self):
+        orderitems=self.orderitem_set.all()
+        total=sum([item.get_total for item in orderitems])
+        return total
+
+
+     @property  # total amt of item quantity
+     def get_cart_items(self):
+        orderitems=self.orderitem_set.all()
+        total=sum([item.quantity for item in orderitems])
+        return total
 
 
 class OrderItem(models.Model):
     product=models.ForeignKey(Product, null=True, blank=True,on_delete=models.SET_NULL)
     order=models.ForeignKey(Order, null=True, blank=True,on_delete=models.SET_NULL)
-    quatity=models.IntegerField(default=0,null=True, blank=True)
+    quantity=models.IntegerField(default=0,null=True, blank=True)
     date_addedd=models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         #print("your id value is "+str(type(str(self.id))))
         return str(self.id)
+    
+    @property  # total amt of order
+    def get_total(self):
+        total=self.product.price*self.quantity
+        return total
 
 class ShippingAddress(models.Model):
     customer=models.ForeignKey(Customer,on_delete=models.SET_NULL,null=True, blank=True)
