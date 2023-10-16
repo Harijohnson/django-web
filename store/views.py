6f88606ba4a41cac670b59c84ca129c5d80c4b45
Harijohnson <harijn72@gmail.com>
2023-10-16 15:46:52 +0530

    new functional changes

--- store/views.py
+++ store/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import *
+from django.http import JsonResponse
+import json
 
 
 # Create your views here.
@@ -30,3 +32,42 @@ def checkout(request):
         order={'get_cart_total':0,'get_cart_items':0}
     context={'items': items,'order':order}
     return render(request,'store/checkout.html',context)
+
+
+def updateItem(request):
+    # data=json.loads(request.data)
+    # productId=data['productId']
+    # action=data['action']
+
+    # print(productId,action)
+    return JsonResponse('item was added successfully',safe=False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
