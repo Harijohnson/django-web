bc37092022dc09ae402e267e1b33991a4bdcb50c
Harijohnson <harijn72@gmail.com>
2023-10-17 08:52:07 +0530

    some javascript updates for cart uparrow abd down arrow

--- store/views.py
+++ store/views.py
@@ -19,18 +19,20 @@ def store(request):
         cartItems= order['get_cart_items']
 
     products=Product.objects.all()
-    context={'products': products}
-    #add some git chnages
+    context={'products': products, 'cartItems': cartItems}
     return render(request,'store/store.html',context)
 def cart(request):
     if request.user.is_authenticated:
         customer=request.user.customer
         order,created=Order.objects.get_or_create(customer=customer, compleate=False)
         items=order.orderitem_set.all()
+        cartItems = order.get_cart_items
     else:
         items=[]
         order={'get_cart_total':0,'get_cart_items':0}
-    context={'items': items,'order':order}
+        cartItems= order['get_cart_items']
+
+    context={'items': items,'order':order,'cartItems': cartItems}
     return render(request,'store/cart.html',context)
 
 def checkout(request):
@@ -38,10 +40,12 @@ def checkout(request):
         customer=request.user.customer
         order,created=Order.objects.get_or_create(customer=customer, compleate=False)
         items=order.orderitem_set.all()
+        cartItems = order.get_cart_items
     else:
         items=[]
         order={'get_cart_total':0,'get_cart_items':0}
-    context={'items': items,'order':order}
+        cartItems= order['get_cart_items']
+    context={'items': items,'order':order,'cartItems': cartItems}
     return render(request,'store/checkout.html',context)
 
 
@@ -49,8 +53,8 @@ def updateItem(request):
     data=json.loads(request.body)
     productId=data['productId']
     action=data['action']
-    print('productId:'+str(productId))
-    print('action:'+str(action))
+    # print('productId:'+str(productId))
+    # print('action:'+str(action))
     # print(productId,action)
 
 
@@ -60,6 +64,7 @@ def updateItem(request):
     order,created=Order.objects.get_or_create(customer=customer, compleate=False)
     
     orderItem,created=OrderItem.objects.get_or_create(order=order, product=product)
+    #print(orderItem)
     
     if action=='add':
         orderItem.quantity=(orderItem.quantity+1)
